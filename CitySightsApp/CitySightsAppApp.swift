33e7a786de4309881ec9662e07ed4858b0dbce1e
Wafi Choudhury <wafster1337@gmail.com>
2022-01-03 18:44:43 -0600

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Wafi Choudhury on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
